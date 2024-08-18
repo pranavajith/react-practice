a13729bccc9daca263af3ce4ff2eb7a981c27bb5
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-08-18 11:58:49 +0530

    Buttons added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import react, {useState} from 'react';
+
+// const products = [
+//   { title: 'Cabbage', id: 1 },
+//   { title: 'Garlic', id: 2 },
+//   { title: 'Apple', id: 3 },
+// ];
+
+// const listsOfProducts = products.map(product => 
+//   (
+//     <li key = {product.id}> {product.title} </li>
+//   )
+// )
+
+// for (const x of listsOfProducts){
+//   console.log(x.length)
+// }
+
+// // console.log(listsOfProducts);
+
+
+const MyButton = ({count, onClick}) => {
+
+  return (
+    <button onClick = {onClick}> Clicked {count} times</button>
+  )
+}
+
+const MyButton2 = () => {
+  const [state, setState] = useState(0);
+  
+  const incr = () => {
+    setState(state+1);
+  }
+  return (
+    <button onClick = {incr}> Clicked {state} times</button>
+  )
+}
+
 
 function App() {
+  const [state, setState] = useState(0);
+  
+  const incr = () => {
+    setState(state+1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    Related Buttons! <br />
+    <MyButton count = {state} onClick = {incr}/> <br />
+    <MyButton count = {state} onClick = {incr}/> <br />
+    Unrelated Buttons! <br />
+    <MyButton2 /> <br />
+    <MyButton2 /> <br />
+    </>
+    
   );
 }
 
